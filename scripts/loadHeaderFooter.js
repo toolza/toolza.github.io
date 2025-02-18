5e448a4e381a85d7f1b1a1014d095cba3f30089f
parvind87 <mail2parvind@gmail.com>
2025-02-18 12:53:29 +0530

    header footer

--- scripts/loadHeaderFooter.js
+++ scripts/loadHeaderFooter.js
@@ -1,14 +1,14 @@
 // Function to load header and footer
 function loadHeaderFooter() {
   // Load Header
-  fetch('header.html')
+  fetch('../../header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
     });
 
   // Load Footer
-  fetch('footer.html')
+  fetch('../../footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer').innerHTML = data;
